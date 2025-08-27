e80264de592ebc06f368b2506c31b60215dc2f25
Didora Qazoqova <didoraqazoqova421@gmail.com>
2025-08-27 14:13:30 +0500

    my homework

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+let role = "Admin";
+let subscripsion = "free";
+let productStatus ="available";
 
+if(role==="Admin"){
+    console.log("Polniy dostup");
+}else if(role==="user") {
+    console.log("Ogranicheny dostp");
+}else if(role==="guest"){
+    console.log("Tolko prosmotr");
+
+}
+
+
+
+switch(subscription){
+    case "free":
+        console.log("Minimum funksiy");
+        break;
+        case "pro":
+        console.log("Polniy dostup");
+        break;
+        case "enterprice":
+        console.log("Vip-poderzhka");
+}
+if(productStatus==="available" && (subscription==="pro" ||subscription==="enterprise")){
+    console.log("Mozhno zakazat");
+}else if(productStatus==="sale"&& (role==="user" ||role==="guest")){
+    console.log("Dostuonaya aksiya");
+
+}else if(productStatus==="out-of-stock"){
+    console.log("Tovar nedostupen");
+}
